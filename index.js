15e94d50d1de421cfdc814dde1e40bca53d8e36c
Freddie16 <freddiemurigi@gmail.com>
2022-11-25 12:52:09 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+const saturdayFun = function (activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = function (activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(take = "#") {
+    const emphatic = function(adjective = "special") {
+        return `You are ${take}${adjective}${take}!`
+    }
+    return emphatic
+}
+   
+
+
